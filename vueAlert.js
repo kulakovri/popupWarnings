c2093a58cf4646bf462d001fc0c5864ee3d5361f
kulakovri <kulakovri@gmail.com>
2020-01-19 17:52:10 +0300

    introduce components

--- vueAlert.js
+++ vueAlert.js
@@ -1,7 +1,7 @@
 const vueAlert = function() {
   return {
     appendVue() {
-      const MyComponent = Vue.extend({
+      const MainComponent = Vue.extend({
         template: `
 <div>
     <h1>Message</h1>
@@ -9,10 +9,11 @@ const vueAlert = function() {
     <br>
     <input v-model="inputMessage">
     <button @click="addAnotherMessage">Add another message</button>
+    <button-counter></button-counter>
 </div>`,
         data() {
           return {
-            listOfMessages: ['message 1', "message 2", "message 3",  "message 4",  "message 5"],
+            listOfMessages: ["message 1", "message 2", "message 3", "message 4", "message 5"],
             inputMessage: ""
           }
         },
@@ -22,7 +23,24 @@ const vueAlert = function() {
           }
         }
       })
-      const component = new MyComponent().$mount()
+
+      Vue.component("button-counter", {
+        data: function() {
+          return {
+            count: 0
+          }
+        },
+        template: `
+        <div class="block">
+        <div class="block__square block__square-pink" onclick="alertPink()"></div>
+        <div class="block__square block__square-red" onclick="alertRed()"></div>
+        <div class="block__square block__square-blue" onclick="alertBlue()"></div>
+        <div class="block__square block__square-green" onclick="alertGreen()"></div>
+        <div class="block__square block__square-yellow" onclick="alertYellow()"></div>
+    </div>
+        `
+      })
+      const component = new MainComponent().$mount()
       document.body.appendChild(component.$el)
     }
   }
